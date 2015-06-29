624e6f9e6051293973cc0e256801a59437331a11
Will Collins IV <bcollinsiv@gmail.com>
2015-06-29 08:07:16 -0400

    Adding utility functions to crdimport

--- Core Reference Data (CRD)/crdimport.js
+++ Core Reference Data (CRD)/crdimport.js
@@ -1,11 +1,14 @@
 // PseudoVet - Core Reference Database Import Utility
-// 
+// crdimpor((t.js
+var js=require("FileReader");
 
-// print process.argv
-//
 console.log("PseudoVet - Core Reference Database Import");
 console.log("Written by: Will BC Collins IV");
 
+var inputFileName='lastnames.txt';
+var outputFileName='lastnames.json';
+
+// process.argv
 process.argv.forEach(function (val, index, array) {
 	if (index == 2) {
 		var rePattern = new RegExp(/^\-\-help$/);
@@ -14,6 +17,8 @@ process.argv.forEach(function (val, index, array) {
 	}
 	console.log("Fin");
 });
+
+parseFile(inputFileName);
 
 function copyData(savPath, srcPath) {
 	fs.readFile(srcPath, 'utf8', function (err, data) {
@@ -26,14 +31,26 @@ function copyData(savPath, srcPath) {
 	});
 }
 
-function writeFile () {
+// this is a placeholder for writing files
+function writeFile (outputFileName, inputData) {
 	fs.writeFile (savPath, data, function(err) {
 		if (err) throw err;
 		console.log('complete');
-	});	
+	});
 }
 
 // -------------- functions ---------------
+function parseFile(FileName) {
+	var fs=new FileReader();
+	fs.readFile(FileName, 'utf8', function (err, data) {
+		if (err) throw err;
+		var dataArray = data.replace(/(\r\n)|\r|\n/g, '\n').split(/\n+/g);
+		for(var line in dataArray) {
+			console.log(line);
+		}
+	})
+}
+
 function help() {
 	console.log("crdimport.js - converts text files to json");
 	console.log("options:");
