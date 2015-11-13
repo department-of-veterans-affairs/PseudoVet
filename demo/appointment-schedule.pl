26fe708122e4972d56eba854d8c525ec8d1c8ed8
bciv <bciv@Phoenix.local>
2015-11-13 12:12:53 -0500

    seperated configuration settings from demo script for appointment-scheduling

--- demo/appointment-schedule.pl
+++ demo/appointment-schedule.pl
@@ -1,12 +1,17 @@
 #!/usr/bin/perl -w
 use Expect;
 
-my $username='bciv';
-my $password='atomic!!';
-my $servername='pseudovista.vaftl.us';
-my $command="ssh $servername";
+#my $username='bciv';
+#my $password='at0mic!!';
+#my $servername='pseudovista.vaftl.us';
+#my $timeout=1500;
+#my $command="ssh $servername";
 
-my $exp=Expect->spawn($command,@params) or die "Cannot spanw $command: $!\n";
+my $configuration_file='../config/demo.config';
+
+configure($configuration_file);
+
+my $exp=Expect->spawn($command,@params) or die "Cannot spawn $command: $!\n";
 
 $exp->expect($timeout,
   [ qr/The authenticity of host/ => sub {
@@ -19,5 +24,41 @@ $exp->expect($timeout,
     exp_continue;
     }]);
 
+$exp->expect($timeout,
+  [qr//=>sub{
+    my $exp=shift;
+    $exp->send("");
+    exp_continue;
+  }]);
 
+#USER>D ^%CD
+#
+#Namespace: VISTA
+#You're in namespace VISTA
+#VISTA> D ^ZU
+#ACCESS CODE:  PR12345
+#VERIFY CODE: PR12345!!
+#Select Systems Manager Menu <TEST ACCOUNT> Option:
 
+#$exp->expect($timeout,
+#  [qr//=>sub{
+#    my $exp=shift;
+#    $exp->send("");
+#    exp_continue;
+#  }]);
+
+sub xcall{
+  my($expression,$response)=@_;
+  $exp->expect($timeout,
+    [qr/$expression/=>sub{
+      my $exp=shift;
+      $exp->send("$response\n");
+      exp_continue;
+    }]);  
+}
+
+sub configure{
+  my $file=$_[0];
+  open(IN,$file) or die "Cannot read configuration file, $file : $!";
+  while(<IN>){eval $_;} close(IN);
+}
