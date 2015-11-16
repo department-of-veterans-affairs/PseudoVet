e86199918d52f52e29faed2af97610c4b6d84b6e
Will BC Collins IV <bcollinsiv@gmail.com>
2015-11-16 17:58:12 -0500

    demo update 2015-11-16
    Added sub routines to demo script and added expect to vagrant build

---
+++ demo/README.md
@@ -0,0 +1,6 @@
+# Prerequisites on VistA system at Linux level:
+
+~Assuming this is the location of 'csession'
+```
+sudo chgrp users /usr/local/etc/cachesys/csession
+```
--- demo/appointment-schedule.pl
+++ demo/appointment-schedule.pl
@@ -1,5 +1,8 @@
 #!/usr/bin/perl -w
 use Expect;
+$Expect::Debug = 1; # verbose debug
+$Expect::Log_Stdout = 1; # show chatter for debugging
+my $prompt = '\$';
 
 my $configuration_file='../config/demo.config';
 
@@ -7,23 +10,8 @@ configure($configuration_file);
 
 my $exp=Expect->spawn($command,@params) or die "Cannot spawn $command: $!\n";
 
-$exp->expect($timeout,
-  [ qr/The authenticity of host/ => sub {
-    my $exp=shift;
-    $exp->send("y\n");
-    exp_continue;}],
-  [ qr/Password/i => sub {
-    my $exp=shift;
-    $exp->send("$password\n");
-    exp_continue;
-    }]);
-
-$exp->expect($timeout,
-  [qr//=>sub{
-    my $exp=shift;
-    $exp->send("");
-    exp_continue;
-  }]);
+login();
+cmd($exp, "csession cache -U bciv \"^ZU\"\n");
 
 #USER>D ^%CD
 #
@@ -41,6 +29,27 @@ $exp->expect($timeout,
 #    exp_continue;
 #  }]);
 
+# log into server over SSH
+#
+sub login {
+  my $rc=$exp->expect($timeout,
+  [ qr/The authenticity of host/ => sub {
+    my $exp=shift;
+    $exp->send("yes\n");
+    $exp->send("$password\n");
+    exp_continue;}],
+  [ qr/Password/i => sub {
+    my $exp=shift;
+    $exp->send("$password\n");
+    exp_continue;
+    }],
+    -re => $prompt,
+  );
+  return defined($rc) ? $rc : 0;
+}
+
+
+
 sub xcall{
   my($expression,$response)=@_;
   $exp->expect($timeout,
@@ -51,6 +60,17 @@ sub xcall{
     }]);  
 }
 
+sub cmd { my ($exp, $cmd) = @_;
+ $exp->print($cmd, "\n");
+ $exp->expect($timeout, -re => $prompt) or return;
+ my $output = $exp->before;
+ $output =~ s/\r//g;  # silly telnet line endings
+ $output =~ s/^$cmd\n//;  # remove the sent cmd
+ return $output;
+}
+
+# read configuration variables from file
+#
 sub configure{
   my $file=$_[0];
   open(IN,$file) or die "Cannot read configuration file, $file : $!";
