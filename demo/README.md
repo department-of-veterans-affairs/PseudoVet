de40ae1ba45685384a474c6032673d0e85842f0c
bciv <bciv@Phoenix.local>
2015-11-18 19:33:18 -0500

    Updated demo README

--- demo/README.md
+++ demo/README.md
@@ -1,10 +1,13 @@
+# PseudoVet Proof Of Concept
+![PseudoVet Logo](https://github.com/VHAINNOVATIONS/PseudoVet/blob/master/branding/PseudoVet.png)
+
 # Prerequisites to run the appointment-schedule.pl automation script:
 
 # VistA Instance
 There must be a VistA server that you have access to.  This script was written to 
 work against eHMP's SILVER Image of VistA as of 20151118
 
-# Linux OS Level Requirements
+## Linux OS Level Requirements of VistA Server
 The Intersystems Cache' 'csession' command must be accessible for use by 
 the user account that is logged into to access VistA as a terminal application
 so that it is not necessary to use the sudo command to use csession.
@@ -20,7 +23,7 @@ Typically, this will not work:
 sudo chgrp users /usr/local/etc/cachesys/csession
 ```
 
-# .bashrc auto prompt for access/verify code
+## .bashrc auto prompt for access/verify code
 The account that the script SSH's into must additionally be configured to 
 automatically prompt for the access/verify pair.
 
@@ -50,11 +53,37 @@ then
   exit
 fi
 ```
-# VistA Requirements
+## VistA Menu/Key Requirements
 Additionally, the access and verify code used to log into VistA must have 
 options and keys assigned necessary to use the APPOINTMENT MENU's, MAKE 
 APPOINTMENT OPTION
 
---
-Will BC Collins IV, VHA Innovation Laboratory
-william.collins@va.gov
+## Configuration file
+See the sample.demo.config file under the ../config folder
+- copy the sample.demo.config file to demo.config (new file)
+- edit the variables in the file to match your environment
+
+# To run from Mac OS X
+To run from Mac OS X, you must install the Perl Expect module
+
+To setup and schedule an appointment against the demo.config settings:
+```
+sudo perl -MCPAN -e "shell"
+install expect
+cd demo
+perl appointment-schedule.pl
+```
+*Once expect is installed you only need to run the last line:
+```
+perl appointment-schedule.pl
+```
+
+# To run from Vagrant under Windows
+Follow instructions in the README.md from https://github.com/VHAINNOVATIONS/PseudoVet
+or from the cloned or forked PseudoVet repository
+
+From 'vagrant ssh' run the following command:
+```
+cd /vagrant/demo
+perl appointment-schedule.pl
+```
---
+++ demo/physician-assignment.pl
@@ -0,0 +1,191 @@
+#!/usr/bin/perl -w
+use Expect;
+# If you don't know the code, don't mess around below -BCIV
+# Purpose: programatically assign a patient to a primary care provider in the Department 
+# of Veterans Affairs VistA Electronic Health Record System (EHR).
+# Written by: Will BC Collins IV {a.k.a., 'BCIV' follow me on GitHub ~ I'm super cool.}
+# Email: william.collins@va.gov
+# Last Modified: 20151118 unless I forgot to update this line...
+#
+# ^^personal patient list menu
+Select Personal Patient List Menu
+# ad 
+Select Build Patient List Menu 
+# on
+Select PATIENT NAME:
+# lastname,firstname
+CHOOSE
+# (parse to select correct number) select number
+
+$Expect::Debug = 0; # verbose debug
+$Expect::Log_Stdout = 1; # show chatter for debugging
+my %month=('Jan'=>'1','Feb'=>'2','Mar'=>'3','Apr'=>'4','May'=>'5','Jun'=>'6','Jul'=>'7','Aug'=>'8','Sep'=>'9','Oct'=>'10','Nov'=>'11','Dec'=>'12');
+
+# see ../config/sample.demo.config for example
+my $configuration_file='../config/demo.config';
+
+configure($configuration_file);
+
+my $exp=Expect->spawn($command) or die "Cannot spawn $command: $!\n";
+
+$exp->expect($timeout,
+  [ qr/The authenticity of host/ => sub {
+    my $exp=shift; $exp->send("y\n"); exp_continue;}],
+  [ qr/Password/i => sub {
+    my $exp=shift; $exp->send("$password\n"); exp_continue;
+  }],
+  [ qr/ACCESS CODE:/=>sub{
+    my $exp=shift; $exp->send("$access_code\r"); exp_continue;
+  }],
+  [ qr/VERIFY CODE:/=>sub{
+    my $exp=shift; $exp->send("$verify_code\r"); exp_continue;
+  }],
+  [ qr/Select TERMINAL TYPE NAME:/=>sub{
+    my $exp=shift; $exp->send("\r"); exp_continue;
+  }],
+  [ qr/Select Systems Manager Menu <TEST ACCOUNT> Option:/=>sub{
+    my $exp=shift; $exp->send("^^MAS MASTER MENU\r"); exp_continue;
+  }],
+#  [ qr/Select MAS MASTER MENU <TEST ACCOUNT> Option:/=>sub{
+#    my $exp=shift; $exp->send("SD\r"); exp_continue;
+#  }],
+#  [ qr/Select Scheduling Manager's Menu <TEST ACCOUNT> Option:/=>sub{
+#    my $exp=shift; $exp->send("APPOINTMENT MENU\r"); exp_continue;
+#  }],
+#  [ qr/Select Appointment Menu <TEST ACCOUNT> Option:/=>sub{
+#    my $exp=shift; $exp->send("MAKE APPOINTMENT\r"); exp_continue;
+#  }],
+#  [ qr/Select CLINIC:/=>sub{ # next prompt CHOOSE 1-2: had to be entered here...
+#    my $exp=shift; $exp->send("$clinic_name\r1\r"); exp_continue;
+#  }],
+#  [ qr/Select PATIENT NAME:/=>sub{
+#    my $exp=shift; $exp->send("$patient_name\r"); exp_continue;
+#  }],
+#  [ qr/IS THIS APPOINTMENT FOR A SERVICE CONNECTED CONDITION?/=>sub{ # next prompt APPOINTMENT TYPE: REGULAR
+#    my $exp=shift; $exp->send("N\r"); exp_continue;
+#  }],
+#  [ qr/APPOINTMENT TYPE:/=>sub{ # REGULAR
+#    my $exp=shift; $exp->send("REGULAR\r"); exp_continue;
+#  }],
+#  [ qr/Select ETHNICITY:/=>sub{ # who cares
+#    my $exp=shift; $exp->send("\r"); exp_continue;
+#  }],
+#  [ qr/Select RACE:/=>sub{ # who cares
+#    my $exp=shift; $exp->send("\r"); exp_continue;
+#  }],  
+#  [ qr/IS THIS A 'NEXT AVAILABLE' APPOINTMENT REQUEST?/=>sub{
+#    my $exp=shift; $exp->send("Y\r"); exp_continue;
+#  }],
+#  [ qr/DATE\/TIME:/=>sub{
+#    # This is where we send in the micro managers... 
+#    # ...okay so I liked the LEGO movie I'm a dork like who cares.
+#    my $next_available_appointment=get_next_available_appointment($exp->before);
+#    my $exp=shift; $exp->send("$next_available_appointment\r"); exp_continue;
+#  }],
+);
+
+# parse appointment availability table that only makes sense to humans
+# takes $exp->before() as input where $exp->before gives you this: ...
+#
+#                                    PRIMARY CARE
+#                                    Nov 2015
+#
+# TIME  |8      |9      |10     |11     |12     |1      |2      |3      |4      
+# DATE  |       |       |       |       |       |       |       |       |
+#WE 18  [4] [4] [4] [4] [4] [4] [4] [4] [4] [4] [4] [4] [4] [4] [4] [4] [4] [4] 
+#TH 19  [4] [4] [4] [4] [4] [4] [4] [4] [4] [4] [4] [4] [4] [4] [4] [4] [4] [4] 
+#FR 20  [4] [4] [4] [4] [4] [4] [4] [4] [4] [4] [4] [4] [4] [4] [4] [4] [4] [4] 
+#SA 21  [4] [4] [4] [4] [4] [4] [4] [4] [4] [4] [4] [4] [4] [4] [4] [4] [4] [4] 
+#SU 22  [4] [4] [4] [4] [4] [4] [4] [4] [4] [4] [4] [4] [4] [4] [4] [4] [4] [4] 
+#MO 23  [4] [4] [4] [4] [4] [4] [4] [4] |       [4] [4] [4] [4] [4] [4] [4] [4] 
+#TU 24  [4] [4] [4] [4] [4] [4] [4] [4] |       [4] [4] [4] [4] [4] [4] [4] [4] 
+#WE 25  [4] [4] [4] [4] [4] [4] [4] [4] [4] [4] [4] [4] [4] [4] [4] [4] [4] [4] 
+#   26    Thanksgiving Day
+#FR 27  [4] [4] [4] [4] [4] [4] [4] [4] [4] [4] [4] [4] [4] [4] [4] [4] [4] [4] 
+#SA 28  [4] [4] [4] [4] [4] [4] [4] [4] [4] [4] [4] [4] [4] [4] [4] [4] [4] [4] 
+#SU 29  [4] [4] [4] [4] [4] [4] [4] [4] [4] [4] [4] [4] [4] [4] [4] [4] [4] [4] 
+#MO 30  [4] [4] [4] [4] [4] [4] [4] [4] |       [4] [4] [4] [4] [4] [4] [4] [4] 
+#                                    Dec 2015
+#TU 01  [4] [4] [4] [4] [4] [4] [4] [4] |       [4] [4] [4] [4] [4] [4] [4] [4] 
+#
+#60 MINUTE APPOINTMENTS (VARIABLE LENGTH)
+#DATE/TIME:     
+#
+sub get_next_available_appointment{
+  # ...and all who have entered here have never returned...
+  my($table)=@_; my $retval;
+  my @out=split(/\n/,$table);
+  my $aptmonth; my $aptyear;
+  my @hours; # hours of potential available appointments
+  for(my $i=0; $i<@out; ++$i){
+    my $string=$out[$i];
+    # first...
+    # match a MON YEAR such as 'Dec 2015' to build YYYYMM portion of appointment slot
+    if($string=~m/^\s*\w\w\w\s\d\d\d\d\s*+$/){
+      $string=~s/\s*/\^/g; $string=~s/\^\^/,/g; $string=~s/\^//g;
+      my($as,$mon,$year)=split(/,/,$string);
+      $aptyear=$year; $aptmonth=$month{$mon};
+      print "1 $mon $year -> $year$month{$mon}\n";
+    }
+    # second... match this line for available hours
+    # TIME  |8      |9      |10     |11     |12     |1      |2      |3      |4
+    if($string=~m/^\sTIME\s\s\|/){
+      $string=~s/\sTIME\s\s\|//; $string=~s/\s*//g; #strip out TIME and whitespace
+      foreach $hr (split(/\|/,$string)){ push @hours, sprintf("%02d",$hr); }
+      my $display_hours; foreach $hr (@hours){ $display_hours.=" $hr"; }
+      print "\n2 >>> $display_hours\n";
+    }    
+    # third... match for slots that are not taken... 
+    # we take the first available for the appointment
+    # we map it to the corresponding hour based on relation of slot def number
+    # such as [4] for 4 appointment slots per hour... if it is [0] or [X] it is taken already.
+    # WE 18  [4] [4] [4] [4] [4] [4] [4] [4] [4] [4] [4] [4] [4] [4] [4] [4] [4] [4] 
+    # MO 23  [4] [4] [4] [4] [4] [4] [4] [4] |       [4] [4] [4] [4] [4] [4] [4] [4] 
+    if($string=~m/^\w\w\s\d\d\s\s\[/){
+      my $minutes;
+      my $aptday=substr($string,3,2);
+      # get rid of the '[]' brackets leaving us with a space delimited string
+      $string=~s/^\w\w\s//;
+      $string=~s/\[|\]//g;
+      # 0 out blocked out hours from assignment
+      $string=~s/\|\s\s\s\s\s\s/0 0/g;
+      print "\n3 >>> slots: $string\n";
+      my @slots=split(/\s/,$string);
+
+      # there are two of these sets of slots per hour if the number of the slot is greater than 0
+      # as the slots are iterated they begin with 0 for the first slot which is an even number
+      my $myhour=0;
+      for(my $i=0; $i<@slots; $i++){
+        # Simply alternate between even an odd to see if we are at each whole or half hour
+        if(0==$i % 2){ 
+          $minutes="00"; 
+          # if we are past the first hour increment the hour we are looking at
+          if($i>1){++$myhour;} 
+        }else{$minutes="30";}
+        
+        # see if slot is > 0 which would indicate there is an available slot to schedule into
+        if($slots[$i]>0){
+          # We have found an available appointment slot woo hoo!
+          # lets not put it in a log book so we don't get into trouble and end up on the 
+          # news for being total jerks and assign an appointment ... okay that really
+          # isn't funny is it?  What an atrocity.  :(
+          $retval=$aptyear.$aptmonth.$aptday."\@".$hours[$myhour].$minutes;
+          print "$retval\n";
+          # once we have found a free spot this routine will simply return the next available
+          # appointment in the format: YYYYMMDD@HHMM
+          print "\n3 >>> day next available slot is: $retval\n";
+          return $retval;
+        }        
+      }
+    } # end of loop through ^WE 18 [4] [4...
+  } # end of loop through each line of schedule table
+  #return $retval;  
+}
+
+# read configuration variables from file
+#
+sub configure{
+  my $file=$_[0];
+  open(IN,$file) or die "Cannot read configuration file, $file : $!";
+  while(<IN>){eval $_;} close(IN);
+}
